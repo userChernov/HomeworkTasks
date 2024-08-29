ba807c9a246310dea679ba788985659b823ec83a
Anton <a.chernov@icloud.com>
2024-08-29 23:19:30 +0300

    test-commit Hw02-Ex01

---
+++ Hw02/Ex01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+
+            string s1;
+            string s2;
+        }
+    }
+}

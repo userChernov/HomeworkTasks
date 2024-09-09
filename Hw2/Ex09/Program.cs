f137f0fba63d9fb31c377d606530aee81987378f
Anton <a.chernov@icloud.com>
2024-09-09 13:55:57 +0300

    ex09 error fixed

--- Hw2/Ex09/Program.cs
+++ Hw2/Ex09/Program.cs
@@ -4,31 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ex06
+namespace Ex09
 {
     /*
-     * Внутри метода Main() последовательно сложите значения всех переменных и выведите результат в консоль.
+     * Внутри метода Main() в консоль выводится строка, повтори этот вывод еще 7 раз.
+     * 
+     * Хорошее знание и владение инструментами разработки способно существенно повысить твою эффективность.
+     * Многие горячие сочетания клавиш в VS Code работают точно так же, как и в других продуктах корпорации Microsoft.
+     * Но в самой IDE есть ещё огромное количество горячих клавиш и функций, про которые стоит знать любому
+     * разработчику!
+     * 
+     * Одной из таких функций является клонирование текущей строки. Для этого:
+     *      1) Установи указатель строки на строку, которую ты хочешь клонировать.
+     *      2) Нажми сочетания клавиш "Shift ⇧ + Alt + Стрелка вниз ↓" (или "Shift ⇧ + Option ⌥ + Стрелка вниз ↓" на мак).
+     *      3) Шикарно ведь получилось?
+     *  
      */
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            string s1 = "1 ";
-            int a = 0;
-            string s2 = " ";
-            int b = 01;
-            string x = " будет 101";
-
-            /* Добавьте свой код ниже */
-
-            string result = s1 + a;
-            result += s2;
-            result += b;
-            result += x;
-
-            Console.WriteLine(result);
-
+            Console.WriteLine("CTRL + C и CTRL + V - нервно курят в стороне...");
+            Console.WriteLine("CTRL + C и CTRL + V - нервно курят в стороне...");
+            Console.WriteLine("CTRL + C и CTRL + V - нервно курят в стороне...");
+            Console.WriteLine("CTRL + C и CTRL + V - нервно курят в стороне...");
+            Console.WriteLine("CTRL + C и CTRL + V - нервно курят в стороне...");
+            Console.WriteLine("CTRL + C и CTRL + V - нервно курят в стороне...");
+            Console.WriteLine("CTRL + C и CTRL + V - нервно курят в стороне...");
+            Console.WriteLine("CTRL + C и CTRL + V - нервно курят в стороне...");
         }
     }
 }
+

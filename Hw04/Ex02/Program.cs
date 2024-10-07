896b61f3d6d4c41549f5259c300638c82412102b
Anton <a.chernov@icloud.com>
2024-10-08 01:46:50 +0300

    Hw04

---
+++ Hw04/Ex02/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02
+{
+    /*
+     * Проанализируйте шаблон и определите, что должна делать программа.
+     * Реализуйте метод SheepCalculator(), который при каждом вызове должен:
+     *   1) Увеличивать на единицу значение параметра sheepCount.
+     *   2) Подсчитывать общую стоимость всех созданных овец.
+     * Остальные данные исходного шаблона изменять нельзя.
+     * Метод Main() должен вывести в консоль число созданных овец и их суммарную стоимость.
+     */
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                new Sheep().SheepCalculator(i * 100);
+            }
+            Console.WriteLine("Количество пробегавших овечек: " + Sheep.sheepCount + "...");
+            Console.WriteLine("А их суммарная стоимость: " + Sheep.sheepPrice + " условных единиц!");
+        }
+
+        public class Sheep
+        {
+            public static int sheepCount = 0;
+            public static int sheepPrice = 0;
+
+            public void SheepCalculator(int sheepPriceOne)
+            {
+                /* Добавьте свой код ниже */
+                sheepCount ++;
+                sheepPrice += sheepPriceOne;
+
+            }
+        }
+    }
+}
---
+++ Hw04/Ex03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03
+{
+    /*
+     * Добавьте в класс Program статический метод UniversalPrinter(), 
+     * который выводит на экран переданную строку такое количество раз, которое передается ему в качестве параметра. 
+     * Каждый вывод с новой строки. Внутри метода Main() вызовите метод UniversalPrinter(), 
+     * передав ему строку "Красота" и число 5 в качестве аргументов.
+     */
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* Добавьте свой код ниже */
+            UniversalPrinter("Красота", 5);
+            
+        }
+        /* Добавьте свой код ниже */
+
+        public static void UniversalPrinter(string message, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}

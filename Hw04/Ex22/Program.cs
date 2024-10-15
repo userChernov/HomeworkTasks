4a73e1fdd4edd71ef482606a9799a2b01d9549e8
Anton <a.chernov@icloud.com>
2024-10-15 10:29:22 +0300

    Hw04

---
+++ Hw04/Ex22/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex22
+{
+    /*
+     * Внутри метода Main() выведите в консоль числа от 1 до 100, каждое число с новой строки.
+     */
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* Добавьте свой код ниже */
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i+1);
+            }
+        }
+    }
+}
---
+++ Hw04/Ex27/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex27
+{
+    /*
+     * Внутри метода Main() считайте с клавиатуры строку, а затем выведите её в консоль в верхнем регистре 7 раз,
+     * каждый раз с новой строки.
+     */
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* Добавьте свой код ниже */
+            string str = Console.ReadLine();
+
+            for (int i = 0; i < 8; i++)
+            {
+                Console.WriteLine(str.ToUpper());
+            }
+
+        }
+    }
+}

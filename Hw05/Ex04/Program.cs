3b8f3d8d2f173b7b951faf250e9c589ca80cf6fd
Anton <a.chernov@icloud.com>
2024-10-19 19:52:18 +0300

    Hw05

---
+++ Hw05/Ex04/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04
+{
+    /*
+     * Добавьте для класса Program публичную целочисленную переменную id.
+     * Добавьте в класс конструктор, который принимает параметр и устанавливает полученное значение переменной id.
+     */
+    public class Program
+    {
+        /* Добавьте свой код ниже */
+        public int id;
+
+        public Program(int id)
+        {
+            this.id = id;
+        }
+
+        public static void Main(string[] args)
+        {            
+        }
+    }
+}

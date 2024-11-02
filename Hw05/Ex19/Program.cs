a50dca533791af53cf9e88f98226ebe2ffb0e764
Anton <a.chernov@icloud.com>
2024-11-02 12:16:18 +0300

    hw05

---
+++ Hw05/Ex19/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex19
+{
+    /*
+     * Измените класс Program таким образом, чтобы вместо публичных переменных класса, класс стал использовать
+     * автоматические свойства, с соответствующими именами. Ненужные переменные - удалите.
+     * Остальные элементы шаблона изменять нельзя.
+     */
+
+    public class Program
+    {
+        public int Version { get; set; }
+        public string Name { get; set; }
+        public Human Owner { get; set; }
+        public static void Main(string[] args)
+        {
+            /* Добавьте свой код ниже */
+        }
+    }
+
+    public class Human
+    {
+        public string name;
+    }
+}

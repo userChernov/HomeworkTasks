2fcd08985ddea4cbee8688b1641932b28938add2
Anton <a.chernov@icloud.com>
2024-11-12 11:12:51 +0300

    hw05

---
+++ Hw05/Ex30/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex30
+{
+    /*
+     * Создайте публичный статический класс SupportPrinterClass в том же файле и namespace что и класс Program.
+     * Добавьте в созданный класс 4 публичных метода:
+     *      1) Printer, который должен принимать в качестве входящего параметра строку и выводить её на экран 1 раз.
+     *      2) DoublePrinter, который должен принимать в качестве входящего параметра строку и выводить её на экран 2 раза.
+     *      3) TriplePrinter, который должен принимать в качестве входящего параметра строку и выводить её на экран 3 раз.
+     *      4) MultiPrinter, который должен принимать в качестве входящего параметра строку и число раз, которое её надо вывести на экран.
+     */
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SupportPrinterClass.Printer("Printer");
+            Console.WriteLine("=====================");
+            SupportPrinterClass.DoublePrinter("DoublePrinter");
+            Console.WriteLine("=====================");
+            SupportPrinterClass.TriplePrinter("TriplePrinter");
+            Console.WriteLine("=====================");
+            SupportPrinterClass.MultiPrinter("MultiPrinter", 6);
+        }
+    }
+    /* Добавьте свой код ниже */
+
+    public static class SupportPrinterClass
+    {
+        public static void Printer(string toPrinter)
+        {
+            Console.WriteLine(toPrinter);
+        }
+        public static void DoublePrinter(string toPrinter)
+        {
+            Console.WriteLine(toPrinter);
+            Console.WriteLine(toPrinter);
+        }
+        public static void TriplePrinter(string toPrinter)
+        {
+            for (int i = 0; i < 3; i++)
+                Console.WriteLine(toPrinter);
+        }
+        public static void MultiPrinter(string toPrinter, int printCount)
+        {
+            for (int i = 0; i < printCount; i++)
+                Console.WriteLine(toPrinter);
+        }
+
+    }
+}
---
+++ Hw05/Ex31/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex31
+{
+    /*
+     * Для класса Program создайте статический конструктор, который бы устанавливал его полям следующие значения:
+     *      1)isOn = true.
+     *      2)line = "It's gonna be fantastic!".
+     *      3)goal = 10101.
+     * Шаблон задачи изменять нельзя.
+     */
+
+    public class Program
+    {
+        public static bool isOn;
+        public static string line;
+        public static int goal = 100001;
+
+        /* Добавьте свой код ниже */
+        static Program()
+        {
+            isOn = true;
+            line = "It's gonna be fantastic!";
+            goal = 10101;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(isOn);
+            Console.WriteLine(line);
+            Console.WriteLine(goal);
+        }
+    }
+}

dced15e026a416f8cca116a157a6643049366fd3
Anton <a.chernov@icloud.com>
2024-11-26 16:57:27 +0300

    Ex10

---
+++ Hw06/Ex10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l6t10
+{
+    /*
+     * Измените шаблон таким образом, чтобы безопасно парсить число из строки, используя метод TryParse структуры Int32.
+     * Если введено число - выведите его на экран, в противном случае выведите на экран фразу "Ошибка парсинга!".
+     */
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int a;
+            bool isInt = Int32.TryParse(s, out a);
+
+            if (isInt)
+                Console.WriteLine(a);
+            else
+                Console.WriteLine("Ошибка парсинга!");
+        }
+    }
+}

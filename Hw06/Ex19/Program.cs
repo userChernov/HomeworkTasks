e782f136ff71b95913b35794f534094a1a834cf9
Anton <a.chernov@icloud.com>
2024-12-01 19:04:21 +0300

    Ex19

--- Hw06/Ex19/Program.cs
+++ Hw06/Ex19/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l6t19
+{
+    /*
+     * Реализуйте статический метод WordSorter() который:
+     *      1) Принимает строку произвольной длины.
+     *      2) Формирует и возвращает новую строку так, чтобы буквы каждого слова принятой строки были отсортированы по алфавиту.
+     *      3) Если слово содержит буквы не русского алфавита или цифры - оно должно остаться без изменения.
+     * Шаблон задачи изменять нельзя.
+     */
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string s1 = "Красивая строка с1223 123";
+            string s2 = "Один два три";
+            string s3 = "Арбу3";
+            Console.WriteLine(WordSorter(s1));
+            Console.WriteLine(WordSorter(s2));
+            Console.WriteLine(WordSorter(s3));
+        }
+
+        public static string WordSorter(string word)
+        {
+            /* Добавьте свой код ниже */
+            string sort;
+            bool needSort = true;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (char.IsDigit(word[i]))
+                {
+                    needSort = false;
+                    break;
+                }
+            }
+
+            if (!needSort) 
+                return word;
+
+            return null;
+            
+        }
+    }
+}

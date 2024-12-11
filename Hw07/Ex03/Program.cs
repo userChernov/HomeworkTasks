7f0829dba901432ed5e40176643077b264d514de
Anton <a.chernov@icloud.com>
2024-12-11 11:26:00 +0300

    Ex03

---
+++ Hw07/Ex03/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l7t3
+{
+    /*
+     * На вход в метод Main() будет подан массив строк args, каждая из строк будет содержать число типа double в строковом представлении, с различным количеством знаков после точки.
+     * Создайте статический метод SmartCutter() который принимает массив строк и возвращает динамический массив, содержащий только те числа double из принятого массива, 
+     * в которых количество знаков после точки меньше либо равно 3. 
+     * Внутри метода Main() выведите результат работы метода SmartCutter() в консоль, каждый элемент с новой строки.
+     */
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* Добавьте свой код ниже */
+        }
+
+        public static ArrayList SmartCutter(string[] array)
+        {
+            ArrayList returnArray = new ArrayList();
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                
+            }
+
+            return null;
+        }
+    }
+}
+

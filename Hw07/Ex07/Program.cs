ca70fdb100404568dacce1417b1995a2b0edc69a
Anton <a.chernov@icloud.com>
2024-12-13 13:04:09 +0300

    Ex07

--- Hw07/Ex07/Program.cs
+++ Hw07/Ex07/Program.cs
@@ -21,11 +21,15 @@ namespace l7t7
             List<string> list = new List<string>() { "строка1", "строка21", "строка31", "строка41", "строка51", "строка61", "строка71", "строка81", "строка91", "строка101" };
             /* Добавьте свой код ниже */
 
-            List<string>.Enumerator it = list.GetEnumerator();
-
-            while (it.MoveNext())
+            for (int i = 0; i < 17; i++)
             {
-                Console.WriteLine(it.Current);
+                list.Insert(0, list[list.Count - 1]);
+                list.RemoveAt(list.Count - 1);
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
             }
         }
     }

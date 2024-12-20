ef15b64dd199b23c9ef40873b42c61f791bde2d1
Anton <a.chernov@icloud.com>
2024-12-20 22:14:48 +0300

    Ex22

---
+++ Hw07/Ex22/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l7t22
+{
+    /*
+     * Реализуйте статический метод GetSeconds() который должен:
+     *      1) Принимать два объекта типа DateTime.
+     *      2) Возвращать количество милисекунд, которое прошло между двумя принятыми датами.
+     * Шаблон задачи изменять нельзя.
+     */
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine(GetSeconds(now, now.Add(new TimeSpan(10000))));
+            Console.WriteLine(GetSeconds(now, now.Add(new TimeSpan(20000))));
+            Console.WriteLine(GetSeconds(now, now.Add(new TimeSpan(50000))));
+        }
+
+        public static long GetSeconds(DateTime dateA, DateTime dateB)
+        {
+            /* Добавьте свой код ниже */
+            
+            return (dateB - dateA).Milliseconds;
+        }
+    }
+}
+

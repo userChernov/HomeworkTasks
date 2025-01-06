66aa22bc59ad73cc2a7b95a53afb31f3c67130b2
Anton <a.chernov@icloud.com>
2025-01-06 22:18:37 +0300

    CorrectEx13

--- Hw06/Ex13/Program.cs
+++ Hw06/Ex13/Program.cs
@@ -33,7 +33,7 @@ namespace l6t13
 
         public static double GetDistance(double x1, double y1, double x2, double y2)
         {
-            return Math.Sqrt( Math.Pow((x2-x1), 2) - Math.Pow((y2-y1), 2) );
+            return Math.Sqrt( Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2) );
         }
 
     }

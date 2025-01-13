43472a6b18142b1c41f4c6179ab976b6ca1c21db
Anton <a.chernov@icloud.com>
2025-01-13 12:21:58 +0300

    Ex19

---
+++ Hw08/Ex21/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace l8t21
+{
+    /*
+     * Создайте метод GetAllSum(), который принимает список массива целых чисел, а возвращает
+     * произведение полных сумм всех его элементов.
+     */
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+        /* Добавьте свой код ниже */
+
+
+    }
+}

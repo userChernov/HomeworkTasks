0d1c090eeab4169d4bfbf04e05094d40853df40b
Anton <a.chernov@icloud.com>
2025-02-07 01:52:53 +0300

    Ex24

---
+++ Hw09/Ex24/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l9t24
+{
+    /*
+     * Внутри метода Main() объявите новый экземпляр класса Stack и сохраните его в отдельную переменную.
+     * Добавьте в Stack 5 строчек, считанных с клавиатуры, после чего выведите на экран все содержимое стека, каждый элемент с новой строки.
+     */
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* Добавьте свой код ниже */
+
+            Stack<string> stack = new Stack<string>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                stack.Push(Console.ReadLine());
+            }
+
+            for (int i = 0; i < stack.Count; i++)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+            
+        }
+    }
+}

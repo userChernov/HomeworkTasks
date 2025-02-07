7a9ddbb1713a258efccf05dc120a28b4d3f6be5e
Anton <a.chernov@icloud.com>
2025-02-07 10:27:19 +0300

    Ex31

---
+++ Hw09/Ex29/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l9t29
+{
+    /*
+     * Внутри класс Program создайте статический метод GetClientList(), который:
+     *      1) Принимает стек экземпляров класса DogClient и стек экземпляров класса CatClient.
+     *      2) Возвращает типизированный стек, содержащий экземлпяры классов DogClient и CatClient расположенные через одного.
+     *      3) Если стек экземпляров классов DogClient больше стека CatClient на 1 или равен ему, то верхним элементом
+     *      возвращаемого стека должен являться экземпляр класса DogClient.
+     *      4) Если стек экземпляров классов DogClient меньше стека CatClient на 1, то верхним элементом
+     *      возвращаемого стека должен являться экземпляр класса CatClient.
+     *      5) Если разница в количестве элементов стеков экземпляров класса DogClient и CatClient больше 2, то
+     *      метод должен вернуть null.
+     * Шаблон задачи изменять нельзя.
+     */
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* Добавьте свой код ниже */
+        }
+        /* Добавьте свой код ниже */
+
+    }
+
+    public class DogClient
+    {
+        public override string ToString()
+        {
+            return "Клиент собачка";
+        }
+    }
+
+    public class CatClient
+    {
+        public override string ToString()
+        {
+            return "Клиент котик";
+        }
+    }
+}
---
+++ Hw09/Ex31/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l9t31
+{
+    /*
+     * Проанализируйте шаблон задачи. Сейчас при выводе информации из стектрейса служебная информация не соответствует действительности.
+     * Определите причину и внесите соответствующие изменения в шаблон так, чтобы служебная информация стала отображаться корректно.
+     */
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Call();
+        }
+
+        public static void Call()
+        {
+            Say();
+        }
+
+        public static void Say()
+        {
+            StackTrace currentStack = new StackTrace(true);
+            for (int i = 0; i < currentStack.FrameCount; i++)
+            {
+                StackFrame stackFrame = currentStack.GetFrame(i);
+                Console.WriteLine("Привет! Я метод - " + stackFrame.GetMethod().Name + "! Я располагаюсь на строке: " + stackFrame.GetFileLineNumber());
+            }
+        }
+    }
+}

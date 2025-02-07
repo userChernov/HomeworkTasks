ab1bece8b826e2c62df060b8981876c1cebe1090
Anton <a.chernov@icloud.com>
2025-02-07 10:35:03 +0300

    Ex32_33

---
+++ Hw09/Ex32/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace l9t32
+{
+    /*
+     * Выведите информацию для кадра метода Say в следующем формате:
+     * "Я метод XXX и я нахожусь в файле: YYY", где XXX - имя метода, а YYY - полное имя файла, в котором он объявлен. 
+     * Изменять сигнатуру созданных методов или менять последовательность их вызова нельзя.
+     */
+
+    public class Program
+    {
+        public static void Main()
+        {
+            I();
+        }
+
+        public static void I()
+        {
+            Just();
+        }
+
+        public static void Just()
+        {
+            Call();
+        }
+
+        public static void Call()
+        {
+            To();
+        }
+
+        public static void To()
+        {
+            Say();
+        }
+
+        public static void Say()
+        {
+            Hello();
+        }
+
+        public static void Hello()
+        {
+            StackTrace currentStack = new StackTrace(true);
+            for (int i = 0; i < currentStack.FrameCount; i++)
+            {
+                StackFrame stackFrame = currentStack.GetFrame(i);
+                Console.WriteLine($"Я метод {stackFrame.GetMethod().Name} и я нахожусь в файле {stackFrame.GetFileName()}");
+            }
+        }
+    }
+}
---
+++ Hw09/Ex33/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l9t33
+{
+    /*
+        * Расставьте в шаблоне задачи вызовы методов таким образом, чтобы в консоль была выведена знаменитая
+        * английская поговорка "В гостях хорошо, а дома лучше" на языке оригинала. 
+        * Остальные элементы шаблона задачи изменять нельзя.
+        */
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            East();
+        }
+
+        public static void Best()
+        {
+            StackTrace currentStack = new StackTrace();
+            for (int i = currentStack.GetFrames().Length - 2; i >= 0; i--)
+            {
+                Console.Write(currentStack.GetFrames()[i].GetMethod().Name + " ");
+            }
+        }
+
+        private static void West()
+        {
+            /* Добавьте свой код ниже */
+            Home();
+        }
+
+        internal static void Or()
+        {
+            /* Добавьте свой код ниже */
+            West();
+        }
+
+        protected static void East()
+        {
+            /* Добавьте свой код ниже */
+            Or();
+        }
+
+        internal protected static void Is()
+        {
+            /* Добавьте свой код ниже */
+            Best();
+        }
+
+        static void Home()
+        {
+            /* Добавьте свой код ниже */
+            Is();
+        }
+    }
+}

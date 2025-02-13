10be77e81c89b19e125705badd07a1cfb97e8b12
Anton <a.chernov@icloud.com>
2025-02-14 01:05:49 +0300

    Ex22_Ex23_Ex24_Ex25

---
+++ Hw10/Ex24/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l10t24
+{
+    /*
+     * Добавьте 1 операцию по преобразованию типов, чтобы на экран было выведено значение 5.5.
+     * Значения переменных изменять нельзя.
+     */
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* Добавьте свой код ниже */
+            int a = 5;      //5
+            int b = 4;      //4
+            int c = 3;      //3
+            int e = 2;
+            double d = a + (double)(b / c) / e;
+            Console.WriteLine(d);
+        }
+    }
+}
+
